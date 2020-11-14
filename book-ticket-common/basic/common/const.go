1badfa61dc1a22702e7c9e8855fc5ff1b33e72a8
sumflowns <72845467+sumflowns@users.noreply.github.com>
2020-11-14 10:04:28 +0800

    更新 const.go

--- book-ticket-common/basic/common/const.go
+++ book-ticket-common/basic/common/const.go
@@ -8,6 +8,6 @@ const (
 	Topic_Product_Es_Add           = "com.surprise.shop.topic.product.add"
 	Topic_Product_Es_Delete        = "com.surprise.shop.topic.product.delete"
 	Topic_Product_Es_Update        = "com.surprise.shop.topic.product.update"
-	ControlCenterAddress           = "0.0.0.0"
+	ControlCenterAddress           = "192.168.10.101"
 	POST_CHECK_CODE_FROM_MY_SERVER = "http://192.168.10.101:5002/verify/base64"
 )
