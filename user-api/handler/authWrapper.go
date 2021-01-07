72745de84cf27f30149397167e93064503c33e41
sumflowns <72845467+sumflowns@users.noreply.github.com>
2021-01-07 16:14:31 +0800

    Update authWrapper.go

--- user-api/handler/authWrapper.go
+++ user-api/handler/authWrapper.go
@@ -20,14 +20,14 @@ type Subject struct {
 func AuthWrapper(c *gin.Context) {
 	log.Logf("[AuthWrapper]:请求 URL: %v", c.Request.RequestURI)
 
-/*
+
 	//调试
 	var url = []string{"/api/user/code","/api/user/login","/user/code","/user/login"} //c.Request.RequestURI 白名单 不需要传token参数的url
 	common.WhileList = &common.UrlWhileList{  Url_whitelist: url,
 	}
 	log.Logf("请求路径",c.Request.RequestURI)
 	log.Logf("WhileList",common.WhileList)
-*/
+
 	if common.WhileList.IsInWileList(c.Request.RequestURI) {
 		//假如该请求是登陆后
 		ck := c.Request.Header.Get(common.RememberMeCookieName)
